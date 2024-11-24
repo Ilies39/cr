020294d6356eebab25e83f01e9d6823dd95a4db1
Ilies39 <hamidi.ilies@gmail.com>
2024-11-24 02:49:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,46 @@
-var ws = null;
-var توقعاتسابقة = []; // تخزين القيم السابقة
-var currentIndex = 0; // فهرس القيمة الحالية
+// إعداد العناصر الديناميكية
+document.addEventListener('DOMContentLoaded', function() {
+    // فتح اتصال WebSocket
+    var ws = null;
+    var توقعاتسابقة = [];
+    var currentIndex = 0;
 
-function openWebSocket() {
-    var url = 'wss://eg1xbet.com/games-frame/sockets/crash?whence=50&fcountry=8&ref=1&gr=0&appGuid=games-web-master&lng=fr&access_token=YOUR_ACCESS_TOKEN';
-    ws = new WebSocket(url);
-    
-    ws.onopen = function() {
-        console.log('WebSocket opened');
-        ws.send('{"protocol":"json","version":1}\x1e');
-        ws.send('{"arguments":[{"activity":30,"account":972708725}],"invocationId":"0","target":"Account","type":1}\x1e');
-    };
+    function openWebSocket() {
+        var url = 'wss://eg1xbet.com/games-frame/sockets/crash?whence=55&fcountry=8&ref=1&gr=1557&appGuid=games-web-master&lng=en&access_token=eyJhbGciOiJFUzI1NiIsImtpZCI6IjEiLCJ0eXAiOiJKV1QifQ.eyJzdWIiOiI1MC8xMDY1NDc1MDI3IiwicGlkIjoiMSIsImp0aSI6IjAvYzFiMWQ4YmQzMTA0NjgzMzY1Zjc4NzMwN2MwOTFmNDM5ZDU1MzY4OWUzZmNkNWMxZWE3Yzk5MGEyZDJhMzc1OSIsImFwcCI6Ik5BIiwiaW5uZXIiOiJ0cnVlIiwibmJmIjoxNzMyNDEwNzY2LCJleHAiOjE3MzI0MjUxNjYsImlhdCI6MTczMjQxMDc2Nn0.TlFT3z2PGCsylSzQUN9QJpw7pC0DyiVMfJ4PCt6lDWnWsbi7P2eYeJB3jHeo_q3gFhg-oZnu38G3jkBgmp5X9A';
+        ws = new WebSocket(url);
+        
+        ws.onopen = function() {
+            console.log('WebSocket opened');
+            ws.send('{"protocol":"json","version":1}\x1e');
+            ws.send('{"arguments":[{"activity":30,"account":972708725}],"invocationId":"0","target":"Account","type":1}\x1e');
+        };
 
-    ws.onclose = function() {
-        console.log('WebSocket closed');
-        ws = null;
-    };
-    
-    ws.onmessage = function(event) {
-        var data = JSON.parse(event.data.slice(0, -1));
-        if (data.target === 'OnCrash') {
-            توقعاتسابقة.push(data.arguments[0].f);
-            عرضالتوقعالتالي(); // عرض القيمة التالية
-        }
-    };
-    
-    ws.onerror = function(event) {
-        console.error('WebSocket error:', event);
-    };
-}
-
-function عرضالتوقعالتالي() {
-    if (currentIndex < توقعاتسابقة.length) {
-        var crashValueElement = document.getElementById('crash-value');
-        crashValueElement.innerText = توقعاتسابقة[currentIndex];
-        currentIndex++;
+        ws.onclose = function() {
+            console.log('WebSocket closed');
+            ws = null;
+        };
+        
+        ws.onmessage = function(event) {
+            var data = JSON.parse(event.data.slice(0, -1));
+            if (data.target === 'OnCrash') {
+                توقعاتسابقة.push(data.arguments[0].f);
+                عرضالتوقعالتالي();
+            }
+        };
+        
+        ws.onerror = function(event) {
+            console.error('WebSocket error:', event);
+        };
     }
-}
 
-// فتح الاتصال
-openWebSocket();
+    function عرضالتوقعالتالي() {
+        if (currentIndex < توقعاتسابقة.length) {
+            var crashValueElement = document.getElementById('crash-value');
+            crashValueElement.innerText = توقعاتسابقة[currentIndex];
+            currentIndex++;
+        }
+    }
+
+    // فتح الاتصال
+    openWebSocket();
+});
